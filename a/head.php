29a5bfa4e0857ff4c15bdd8e523e15324bd43998
yugchak <afkid@hotmail.com>
2020-03-03 20:00:54 +0800

    上传

---
+++ a/head.php
@@ -0,0 +1,32 @@
+﻿<style type="text/css">
+			table{ font-size:12px;}
+</style>
+<table border="0" width="100%" cellpadding="2" height="12">
+    <tr>
+        <td width="100%"><p align="center">
+            <img border="0" src="images/logo.gif" width="400" height="40" alt="留言版"></td>
+    </tr>
+    <tr>
+        <td width="100%" height="6"><p align="center">
+                <font color="#8EB4D9">[</font>
+                <a href="input.php"><font color="#008000">签写留言</font></a>
+                <font color="#8EB4D9">]</font>
+
+                <font color="#8EB4D9">[</font>
+                <a href="browse.php"><font color="#008000">查看留言</font></a>
+                <font color="#8EB4D9">]</font>
+
+                <font color="#8EB4D9">[</font>
+                <a href="register.php"><font color="#008000">用户注册</font></a>
+                <font color="#8EB4D9">]</font>
+
+                <font color="#8EB4D9">[</font>
+                <a href="userlogin.php"><font color="#008000">用户登录</font></a>
+                <font color="#8EB4D9">]</font>
+
+                <font color="#8EB4D9">[</font>
+                <a href="adminlogin.php"><font color="#008000">版主管理</font></a>
+                <font color="#8EB4D9">]</font>
+        </td>
+    </tr>
+</table>                            
---
+++ a/index.php
@@ -0,0 +1,14 @@
+﻿
+<HTML>
+    <HEAD>
+        <TITLE></TITLE>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </HEAD>
+
+    <BODY>
+       
+
+        <h1 align="center"><font color=blue >welcome</font></h1>
+
+    </BODY>
+</HTML>
